c9680b074a9f22bbec3529a6c75ef00fbe60ec06
ovidiucs <ovidiu.ctin@gmail.com>
2014-04-16 13:33:43 +0300

    app.configure no longer required

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
  * Module dependencies.
  */
 
-var express = require('express')
-	,http		= require('http');
+var express = require("express")
+	,http		= require("http");
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+   app.set("port", process.env.PORT || 3000);
+   app.set("views", __dirname + "/views");
+   app.set("view engine", "jade");
+   // Middleware - order is important
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(app.router);
+   app.use(express.static(__dirname + "/public"));
+
 
 app.get("/", function (req,res) {
-   res.render("home.jade",{ title:"Having Fun with Express"});
+   res.render("home",{ title:"Having Fun with Express"});
 });
 
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function(){
+  console.log("Express server listening on port " + app.get("port"));
 });
